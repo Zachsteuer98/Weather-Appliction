6e0221a8845e66a074d5a657f263c6a01b2c5f6a
Zachsteuer98 <Zach.Steuer98@gmail.com>
2022-01-08 20:53:02 -0500

    created todays content. need to add moment.js to display todays date and change the dynamic content to not repeat (delete when new city is entered)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,27 @@
 var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector("#citySearch")
 var citySearchName = document.querySelector("#city-search-name")
+var listGroupEl= document.getElementById("currentDayContent")
 
 var getCurrentWeatherData = function(name) {
     //format the github apiUrl to grab weather for a specific city
     var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + name + "&appid=642eea345e5cbf72aef7bc5c87e8b7e2" 
 
     //make a request to the url
-    fetch(apiUrl).then(function(response) {
-    response.json().then(function(data) {
-    DisplayCity(data, name)
+    fetch(apiUrl)
+    .then(function(response) {
+    response.json().then(function(data) { 
+    const  {name} = data
+    const {speed} = data.wind
+    const  {icon} = data.weather[0]
+    const {temp, humidity} = data.main
+    console.log(data)
+    console.log (temp)
+    console.log(humidity)
+    DisplayCity(name, temp, speed, humidity)
+    // var temp = wind
+
+    // console.log(DisplayCity(data.name))
 });
 });
 };
@@ -29,9 +41,25 @@ var formSubmitHandler = function(event) {
     // console.log(event);
 };
 
-var DisplayCity = function(name) {
+
+var DisplayCity = function(name, temp, wind, humidity) {
     citySearchName.textContent = name
-    console.log(name);
+    // console.log(name);
+var temperature = document.createElement("li");
+temperature.textContent = "Temp:" + temp + "F";
+listGroupEl.appendChild(temperature);
+var windSpeed = document.createElement("li");
+windSpeed.textContent = "Wind:" + wind + "MPH";
+listGroupEl.appendChild(windSpeed);
+var humidityOutside = document.createElement("li");
+humidityOutside.textContent = "Humidity:" + humidity + "%";
+listGroupEl.appendChild(humidityOutside);
+
+// listGroupEl.appendChild(temperature)
+// listGroupEl.appendChild(windSpeed)
+// listGroupEl.appendChild(humidityOutside)
 }
 
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+
